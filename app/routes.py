7ab22bfc1493eeaae8f552f1e03f356ef24253df
Gilberto Hideaki Matsunaga <gmatsuna@gmail.com>
2024-05-11 17:58:37 -0300

    audio issue

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,8 @@
+# Interpretador de fotos de cardápio.
+# Em 4 anos morando no Japão sempre me deparei com a situação de ir ao restaurante e mesmo com a ajuda aplicativos de tradução continuar sem saber do que se trata o prato mostrado no cardápio.
+# Como a grande maioria dos cardápios dos restaurantes costumam ter fotos, este interpretador de fotos de cardápio, com o auxílio do Gemini AI vai nos auxiliar a entender e escolher melhor o que podemos pedir nos restaurantes onde não conseguimos ler o cardápio.
+# Além disso implementei o recurso de texto para áudio para que haja um recurso inclusivo e deficientes visuais ou pessoas com dificuldades para enchergar também possam utilizar o Interpretador desde que alguém o ajude a obter uma foto do cardápio.
+
 import google.generativeai as genai
 import requests
 import pyttsx3
